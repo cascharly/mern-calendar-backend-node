ebf043e2bd84d3ce1b37f8bf47fb8ef11325ec0b
cascharly <cascharly@gmail.com>
2021-03-29 19:37:54 -0300

    08 Endpoints remover, crear y login

--- routes/auth.js
+++ routes/auth.js
@@ -2,13 +2,18 @@
 // host + /api/auth
 
 const { Router } = require("express");
+const {
+  crearUsuario,
+  loginUsuario,
+  revalidarToken,
+} = require("../controllers/auth");
+
 const router = Router();
 
-router.get("/", (req, res) => {
-  //   console.log('Se requiere /');
-  res.json({
-    ok: true,
-  });
-});
+router.post("/new", crearUsuario);
+
+router.post("/", loginUsuario);
+
+router.get("/renew", revalidarToken);
 
 module.exports = router;
