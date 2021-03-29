663da2e26be5192d46c6028fcc9ad50b3a9924a6
cascharly <cascharly@gmail.com>
2021-03-29 19:59:55 -0300

    09 recuperar info de posteo

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ const app = express();
 // Directorio Publico
 app.use(express.static("public"));
 
+// Lectura y parseo del body
+app.use(express.json());
+
 // Rutas
 app.use("/api/auth", require("./routes/auth"));
 // TODO: CRUD: eventos
