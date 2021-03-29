fa5dcedb248ad50bc11fa0d899256222c494c04d
cascharly <cascharly@gmail.com>
2021-03-29 15:30:15 -0300

    05 Configuracion Express

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('Hola Mundo!!!');
+const express = require("express");
+
+// crear servidor de express
+const app = express();
+
+
+//Rutas
+app.get('/', (req,res) => {
+//   console.log('Se requiere /');
+  res.json({
+      ok: true
+  })
+}  ) 
+
+// escuchar peticiones
+app.listen(4000, () => {
+  console.log(`Servidor corriendo en puerto ${4000}`);
+});
