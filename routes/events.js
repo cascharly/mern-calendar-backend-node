09a587ad1f39a30695e7a30b35fb0cc24f07e58e
cascharly <cascharly@gmail.com>
2021-04-06 17:32:24 -0300

    06 Validar Campos Necesarios

--- routes/events.js
+++ routes/events.js
@@ -12,6 +12,9 @@ const {
   actualizarEvento,
   eliminarEvento,
 } = require("../controllers/events");
+const { check } = require("express-validator");
+const { validarCampos } = require("../middlewares/validar-campos");
+const { isDate } = require("../helpers/isDate");
 
 // Todas tienen que validarJWT
 router.use(validarJWT);
@@ -20,7 +23,16 @@ router.use(validarJWT);
 router.get("/", getEventos);
 
 // Crear nuevo evento
-router.post("/", crearEvento);
+router.post(
+  "/",
+  [
+    check('title','El titulo es obligatorio').not().isEmpty(),
+    check('start','Fecha de inicio es obligatoria').custom(isDate),
+    check('end','Fecha de finalizacion es obligatoria').custom(isDate),
+    validarCampos
+  ],
+   crearEvento
+   );
 
 //Actualizar Evento
 router.put("/:id", actualizarEvento);
