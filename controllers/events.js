6ada1819dd24af759f09154c95e3da0e6f7cc626
cascharly <cascharly@gmail.com>
2021-04-06 17:04:54 -0300

    05 Modelo Evento

--- controllers/events.js
+++ controllers/events.js
@@ -7,6 +7,10 @@ const getEventos = (req, res = response) => {
   });
 };
 const crearEvento = (req, res = response) => {
+
+  // verificar que tenga el evento
+  console.log(req.body);
+  
   res.json({
     ok: true,
     msg: "Crear evento",
